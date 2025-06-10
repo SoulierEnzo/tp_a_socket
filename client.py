2dacab1c51cb8b7669a27df23c86c9adf10ee89a
Enzo Soulier <souliere@3il.fr>
2025-06-10 15:27:54 +0200

    Update client.py

--- client.py
+++ client.py
@@ -1,5 +1,7 @@
 import socket
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(("172.16.20.237",63000))
+client.connect(("172.16.21.49",63000))
 client.send("hello la team !".encode())
+client.send("Ceci est un message !".encode())
+client.send("Olala c'est la décadence !".encode())
